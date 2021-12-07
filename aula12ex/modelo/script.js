05c6d10e974c2a19d2985cd36ebe710716fc9643
Carlos André S. Araujo <carlosandresousaaraujo@gmail.com>
2021-12-07 20:21:01 -0300

    aula12

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -1,1 +1,17 @@
-alert('Ola!')
+function carregar () {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora <= 12) {
+        img.src = 'manha.png'
+        document.body.style.background = '#ff913f'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'tarde.png'
+        document.body.style.background = '#603749'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#2F2425'
+    }
+}
